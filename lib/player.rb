e5cc216b208565c9cf5b747e6ccaceceeba5d6cd
gavin.w.cheng <gavin.w.cheng@gmail.com>
2015-09-11 16:52:41 +0100

    Player class built, can fire, take fire, review history of hits and misses

--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,15 @@
 class Player
 
-  attr_reader :board
+  attr_reader :board, :history
 
   def initialize(board)
     @board = board
+    @history = []
   end
 
   def fire(opponent,x,y)
-    opponent.take_fire(x,y)
+    @history << [x,y, opponent.take_fire(x,y)]
+    return history.last[2]
   end
 
   def take_fire(x,y)
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,24 +4,36 @@ describe Player do
 
   subject { Player.new(ShipBoard.new) }
 
-  it "responds to fire with 3 arguments" do
-    expect(subject).to respond_to(:fire).with(3).argument
+  describe ".fire" do
+
+    it "responds to fire with 3 arguments" do
+      expect(subject).to respond_to(:fire).with(3).argument
+    end
+
+    it "calls take_fire on opponent when I fire" do
+      opponent = double :opponent, take_fire: nil
+      expect(opponent).to receive(:take_fire).with(0,0)
+      subject.fire(opponent, 0, 0)
+    end
+
+    it "increases history list by 1" do
+      opponent = double :opponent, take_fire: nil
+      expect { subject.fire(opponent, 0, 0) }.to change { subject.history.length }.by(1)
+    end
+
   end
 
-  it "calls take_fire on opponent when I fire" do
-    opponent = double :opponent, take_fire: nil
-    expect(opponent).to receive(:take_fire).with(0,0)
-    subject.fire(opponent, 0, 0)
-  end
+  describe ".take_fire" do
 
-  it "responds to take_fire with 2 arguments" do
-    expect(subject).to respond_to(:take_fire).with(2).argument
-  end
+    it "responds to take_fire with 2 arguments" do
+      expect(subject).to respond_to(:take_fire).with(2).argument
+    end
 
-  it "returns :hit or :miss or :sunk or :win when I take fire" do
-    hash = {hit: true, miss: true, sunk: true, win: true}
-    expect(hash[subject.take_fire(0,0)]).to be true
-  end
+    it "returns :hit or :miss or :sunk or :win when I take fire" do
+      hash = {hit: true, miss: true, sunk: true, win: true}
+      expect(hash[subject.take_fire(0,0)]).to be true
+    end
 
+  end
 
 end
